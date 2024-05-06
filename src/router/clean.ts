2483adb41818b4d04cddc5e21ba28c28d2bb6c2f
Tony <64509934+oDYRASH@users.noreply.github.com>
2024-05-06 19:40:49 +0200

    init

---
+++ src/router/clean.ts
@@ -0,0 +1,67 @@
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import { useConfigStore } from "@/stores/config";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: () => import("@/views/Home.vue"),
+
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/404",
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to) {
+    // If the route has a hash, scroll to the section with the specified ID; otherwise, scroll to the top of the page.
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 80,
+        behavior: "smooth",
+      };
+    } else {
+      return {
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      };
+    }
+  },
+});
+
+// router.beforeEach((to, from, next) => {
+//   const authStore = useAuthStore();
+//   const configStore = useConfigStore();
+
+//   // current page view title
+//   document.title = `${to.meta.pageTitle} - ${import.meta.env.VITE_APP_NAME}`;
+
+//   // reset config to initial state
+//   configStore.resetLayoutConfig();
+
+//   // // verify auth token before each page change
+//   // authStore.verifyAuth();
+
+//   // before page access check if page requires authentication
+//   if (to.meta.middleware == "auth") {
+//     if (authStore.isAuthenticated) {
+//       next();
+//     } else {
+//       next({ name: "sign-in" });
+//     }
+//   } else {
+//     next();
+//   }
+// });
+
+export default router;
